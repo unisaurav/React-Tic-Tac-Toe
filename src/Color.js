bab6c1503784655a7047a8e3117fab0446f76187
unisaurav <35072056+unisaurav@users.noreply.github.com>
2020-02-10 21:30:19 +0530

    j

--- src/Color.js
+++ src/Color.js
@@ -5,7 +5,9 @@ class Color extends React.Component{
       super();
       this.state={
         player1:'player1',
-        player2:'player2'
+        player2:'player2',
+        p1color:null,
+        p2color:null
       }
 
      
@@ -18,39 +20,54 @@ class Color extends React.Component{
 
     }
 
+    oncolorcall(color,val=2){
+      if(val==1){
+        this.setState({p1color:color})
+      }else{
+        this.setState({p2color:color})
+      }
+
+      console.log(this.state.p1color);
+      console.log(this.state.p2color);
+
+      
+    }
+
+    xoon(data){
+    }
   render(){
     return(
 
       <div style={{display:'flex',justifyContent:'center',marginTop:50}}>
 
       <div>
-        <h2 style={{color:'gray',marginLeft:10}}>player name</h2>
+        <h2 style={{color:'gray',marginLeft:10}}>{this.state.player1}</h2>
         <div style={{display:'flex',flexDirection:'row'}}>
-        <div style={{backgroundColor:'red',height:40,width:40, marginLeft:10}} > </div>
-        <div style={{backgroundColor:'blue',height:40,width:40, marginLeft:10}} > </div>
+        <div style={{backgroundColor:'red',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("red",1)} >{this.xoon("red")} </div>
+        <div style={{backgroundColor:'blue',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("blue")} > </div>
 
-        <div style={{backgroundColor:'green',height:40,width:40, marginLeft:10}} > </div>
+        <div style={{backgroundColor:'green',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("green")}> </div>
 
-        <div style={{backgroundColor:'gray',height:40,width:40, marginLeft:10}} > </div>
+        <div style={{backgroundColor:'gray',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("gray")}> </div>
 
 
-        <div style={{backgroundColor:'yellow',height:40,width:40, marginLeft:10}} > </div>
+        <div style={{backgroundColor:'yellow',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("yellow")}> </div>
 
-        <div style={{backgroundColor:'black',height:40,width:40, marginLeft:10}} > </div>
+        <div style={{backgroundColor:'black',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("black")}> </div>
         </div>
-        <h2 style={{color:'gray',marginLeft:10}}>player name</h2>
+        <h2 style={{color:'gray',marginLeft:10}}>{this.state.player2}</h2>
         <div style={{display:'flex',flexDirection:'row'}}>
-        <div style={{backgroundColor:'red',height:40,width:40, marginLeft:10}} > </div>
-        <div style={{backgroundColor:'blue',height:40,width:40, marginLeft:10}} > </div>
+        <div style={{backgroundColor:'red',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("red")} > </div>
+        <div style={{backgroundColor:'blue',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("blue")} > </div>
 
-        <div style={{backgroundColor:'green',height:40,width:40, marginLeft:10}} > </div>
+        <div style={{backgroundColor:'green',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("green")}> </div>
 
-        <div style={{backgroundColor:'gray',height:40,width:40, marginLeft:10}} > </div>
+        <div style={{backgroundColor:'gray',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("gray")}> </div>
 
 
-        <div style={{backgroundColor:'yellow',height:40,width:40, marginLeft:10}} > </div>
+        <div style={{backgroundColor:'yellow',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("yellow")}> </div>
 
-        <div style={{backgroundColor:'black',height:40,width:40, marginLeft:10}} > </div>
+        <div style={{backgroundColor:'black',height:40,width:40, marginLeft:10}} onClick={()=>this.oncolorcall("black")}> </div>
         </div>
       </div>
 
